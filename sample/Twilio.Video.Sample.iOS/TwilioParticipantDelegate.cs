2650d02d97a1868d18d4988f3b710258da20f05e
Denis Kornev <kdv90@mail.ru>
2017-08-21 18:07:54 +0200

    twilio delegates sample added

---
+++ sample/Twilio.Video.Sample.iOS/TwilioParticipantDelegate.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+using Foundation;
+
+namespace Twilio.Video.Sample.iOS
+{
+    public class TwilioParticipantDelegate :
+            TVIParticipantDelegate
+    {
+        #region Fields
+
+        static TwilioParticipantDelegate _instance;
+
+        #endregion
+
+        #region Properties
+
+        public static TwilioParticipantDelegate Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TwilioParticipantDelegate();
+                }
+
+                return _instance;
+            }
+        }
+
+        public TVIParticipant Participant { get; private set; }
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler<TVIVideoTrack> AddedVideoTrackEvent;
+        void OnAddedVideoTrack(TVIVideoTrack videoTrack)
+        {
+            AddedVideoTrackEvent?.Invoke(this, videoTrack);
+        }
+
+        public event EventHandler<TVIVideoTrack> RemovedVideoTrackEvent;
+        void OnRemovedVideoTrack(TVIVideoTrack videoTrack)
+        {
+            RemovedVideoTrackEvent?.Invoke(this, videoTrack);
+        }
+
+        event EventHandler AddedAudioTrackEvent;
+        void OnAddedAudioTrack()
+        {
+            AddedAudioTrackEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler RemovedAudioTrackEvent;
+        void OnRemovedAudioTrack()
+        {
+            RemovedAudioTrackEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler EnabledTrackEvent;
+        void OntEnabledTrack()
+        {
+            EnabledTrackEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler DisabledTrackEvent;
+        void OnDisabledTrack()
+        {
+            DisabledTrackEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void SetParticipant(TVIParticipant participant)
+        {
+            if (Participant != null)
+            {
+                Participant.Delegate = null;
+            }
+
+            Participant = participant;
+            Participant.Delegate = this;
+        }
+
+        public void Finish()
+        {
+            if (Participant != null)
+            {
+                Participant.Delegate = null;
+            }
+
+            if (_instance != null)
+            {
+                _instance.Dispose();
+                _instance = null;
+            }
+        }
+
+        [Export("participant:addedVideoTrack:")]
+        public override void AddedVideoTrack(TVIParticipant participant, TVIVideoTrack videoTrack)
+        {
+            if (Participant == participant)
+            {
+                OnAddedVideoTrack(videoTrack);
+            }
+        }
+
+        [Export("participant:removedVideoTrack:")]
+        public override void RemovedVideoTrack(TVIParticipant participant, TVIVideoTrack videoTrack)
+        {
+            if (Participant == participant)
+            {
+                OnRemovedVideoTrack(videoTrack);
+            }
+        }
+
+        [Export("participant:addedAudioTrack:")]
+        public override void AddedAudioTrack(TVIParticipant participant, TVIAudioTrack audioTrack)
+        {
+            OnAddedAudioTrack();
+        }
+
+        [Export("participant:removedAudioTrack:")]
+        public override void RemovedAudioTrack(TVIParticipant participant, TVIAudioTrack audioTrack)
+        {
+            OnRemovedAudioTrack();
+        }
+
+        [Export("participant:enabledTrack:")]
+        public override void EnabledTrack(TVIParticipant participant, TVITrack track)
+        {
+            OntEnabledTrack();
+        }
+
+        [Export("participant:disabledTrack:")]
+        public override void DisabledTrack(TVIParticipant participant, TVITrack track)
+        {
+            OnDisabledTrack();
+        }
+
+        #endregion
+    }
+}
---
+++ sample/Twilio.Video.Sample.iOS/TwilioVideoViewDelegate.cs
@@ -0,0 +1,117 @@
+﻿using CoreMedia;
+using Foundation;
+using System;
+
+namespace Twilio.Video.Sample.iOS
+{
+    public class TwilioVideoViewDelegate :
+            TVIVideoViewDelegate
+    {
+        #region Fields
+
+        static TwilioVideoViewDelegate _instance;
+
+        #endregion
+
+        #region Properties
+
+        public static TwilioVideoViewDelegate Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TwilioVideoViewDelegate();
+                }
+
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        public class DimensionsDidChangeEventArgs : EventArgs
+        {
+            public TVIVideoView View { get; private set; }
+            public CMVideoDimensions Dimensions { get; private set; }
+            public DimensionsDidChangeEventArgs(TVIVideoView view, CMVideoDimensions dimensions)
+            {
+                View = view;
+                Dimensions = dimensions;
+            }
+        }
+
+        public event EventHandler<DimensionsDidChangeEventArgs> DimensionsDidChange;
+        void OnDimensionsDidChange(DimensionsDidChangeEventArgs args)
+        {
+            DimensionsDidChange?.Invoke(this, args);
+        }
+
+        public class OrientationDidChangeEventArgs : EventArgs
+        {
+            public TVIVideoView View { get; private set; }
+            public TVIVideoOrientation Orientation { get; private set; }
+            public OrientationDidChangeEventArgs(TVIVideoView view, TVIVideoOrientation orientation)
+            {
+                View = view;
+                Orientation = orientation;
+            }
+        }
+
+        public event EventHandler<OrientationDidChangeEventArgs> OrientationDidChange;
+        void OnOrientationDidChange(OrientationDidChangeEventArgs args)
+        {
+            OrientationDidChange?.Invoke(this, args);
+        }
+
+        public class DidReceiveDataEventArgs : EventArgs
+        {
+            public TVIVideoView View { get; private set; }
+            public DidReceiveDataEventArgs(TVIVideoView view)
+            {
+                View = view;
+            }
+        }
+
+        public event EventHandler<DidReceiveDataEventArgs> DidReceiveData;
+        void OnDidReceiveData(DidReceiveDataEventArgs args)
+        {
+            DidReceiveData?.Invoke(this, args);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Finish()
+        {
+            if (_instance != null)
+            {
+                _instance.Dispose();
+                _instance = null;
+            }
+        }
+
+        [Export("videoView:videoDimensionsDidChange:")]
+        public override void VideoView(TVIVideoView view, CMVideoDimensions dimensions)
+        {
+            OnDimensionsDidChange(new DimensionsDidChangeEventArgs(view, dimensions));
+        }
+
+        [Export("videoView:videoOrientationDidChange:")]
+        public override void VideoView(TVIVideoView view, TVIVideoOrientation orientation)
+        {
+            OnOrientationDidChange(new OrientationDidChangeEventArgs(view, orientation));
+        }
+
+        [Export("videoViewDidReceiveData:")]
+        public override void VideoViewDidReceiveData(TVIVideoView view)
+        {
+            OnDidReceiveData(new DidReceiveDataEventArgs(view));
+        }
+
+        #endregion
+    }
+}
